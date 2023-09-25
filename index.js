df502fe3cac0c00f7d4465c8789b14b2da86aa46
kawatech002 <kawanjiku94@gmail.com>
2023-09-26 01:50:50 +0300

    complrted assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// define variable employee as an object with two keys 
+let employee = {
+    name: "Lucy",
+    streetname: "Jogoo rd"
+};
+ function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let updatedEmployee= {employee, [key]: value};
+    return updatedEmployee
+ }
+ //define destructive function 
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    let employee={[key]: value};
+    return employee
+ }
+//define a function to delet from employee key
+ function deleteFromEmployeeByKey(employee, key) {
+    let newemployee= {employee};
+    delete newemployee[key];
+    return newemployee
+ }
+ //define a function to destructively delete from employee by key
+ function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+
+ }
